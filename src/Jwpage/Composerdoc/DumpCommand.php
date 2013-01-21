b940e17a4468013916f92c7f58be8c2b31ac3034
Johnson Page <jwpage@gmail.com>
2013-01-21 21:16:33 +1000

    add phpdoc

--- src/Jwpage/Composerdoc/DumpCommand.php
+++ src/Jwpage/Composerdoc/DumpCommand.php
@@ -7,11 +7,25 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Command to process a composer.json and composer.lock file to produce
+ * a markdown formatted list of packages, their descriptions and their homepages,
+ * for documentation purposes.
+ */
 class DumpCommand extends Command
 {
+    /**
+     * @var array 
+     */
     protected $lockArray;
+    /**
+     * @var array
+     */
     protected $primaryPackages;
 
+    /**
+     * {@inheritDoc}
+     */
     protected function configure()
     {
         $this
@@ -39,6 +53,9 @@ class DumpCommand extends Command
             ;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->output = $output;
@@ -70,6 +87,16 @@ class DumpCommand extends Command
         }
     }
 
+    /**
+     * Go through the list of packages to write them to the output.
+     * Optionally dumps the first level of subpackages for each required package.
+     * 
+     * @param array $packages 
+     * @param boolean $showSubPackages whether to show subpackages (only goes
+     * one level deep)
+     * @param boolean $isDev whether this is a call for dev packages
+     * @return void
+     */
     protected function dumpPackages($packages, $showSubPackages, $isDev = false)
     {
         $packages = array_keys($packages);
@@ -87,10 +114,17 @@ class DumpCommand extends Command
         }
     }
 
+    /**
+     * Write the details of a particular package to the output.
+     * 
+     * @param array $package 
+     * @param int $indent levels to indent by
+     * @return void
+     */
     protected function writePackage($package, $indent = 0)
     {
-        if (!$package) { // probably "php" requirement
-
+        // If the package is not found, it's probably the "php" requirement.
+        if (!$package) {
             return;
         }
 
@@ -99,6 +133,13 @@ class DumpCommand extends Command
         $this->output->writeln("{$indent}* {$package['name']}: {$package['description']}: {$homepage}");
     }
 
+    /**
+     * Find a required package from the composer.json within the composer.lock file.
+     * 
+     * @param string $packageName 
+     * @param boolean $isDev whether to look inside packages-dev
+     * @return array
+     */
     protected function findPackage($packageName, $isDev = false)
     {
         $flag = $isDev ? 'packages-dev' : 'packages';
